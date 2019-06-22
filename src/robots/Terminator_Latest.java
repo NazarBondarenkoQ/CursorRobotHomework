759f83a4522dd89b7e806c436358ee5ef996ac0e
Nazar Bondarenko <nazarbondarenko1@gmail.com>
2019-06-22 14:33:03 +0300

    Re-uploaded correct files

---
+++ src/robots/Terminator_Latest.java
@@ -0,0 +1,18 @@
+package robots;
+
+    public class Terminator_Latest extends Terminator_Upgrade{
+        private boolean invisibility;
+
+        public Terminator_Latest(String name, String model, String weapon, String form) {
+            super(name, model, weapon, form);
+        }
+
+        public void setInvisibility(){
+            invisibility =! invisibility;
+            if (invisibility == true){
+                System.out.println("I'm invisible");
+            }
+            else
+                System.out.println("I'm visible");
+        }
+    }
---
+++ src/robots/Terminator_Upgrade.java
@@ -0,0 +1,15 @@
+package robots;
+
+public class Terminator_Upgrade extends Terminator{
+    private String form;
+    private String laser = "Laser";
+
+    public Terminator_Upgrade(){
+
+    }
+
+    public Terminator_Upgrade(String name, String model, String weapon, String form){
+        super(name, model, weapon);
+        this.form = form;
+    }
+}
