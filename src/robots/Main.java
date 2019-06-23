fe1e5196d4cbc168ed2b588a6509f341fa3667ad
Nazar Bondarenko <48944609+NazarBondarenkoQ@users.noreply.github.com>
2019-06-23 12:11:58 +0300

    Update Main.java
    Updated main.

--- src/robots/Main.java
+++ src/robots/Main.java
@@ -11,28 +11,23 @@ public class Main {
         int choice = random.nextInt(12) + 1;
 
         Terminator currentTerminator;
+        TerminatorLatest terminatorLatest = new TerminatorLatest("Executor", "T-TX", "Rocket Launcher", "Solid");
+
 
         if (choice <= 3) {
             currentTerminator = new Terminator("Desolator", "T-70", "Shotgun");
-
-            System.out.println("Random number = " + choice + " so created Terminator is: " + currentTerminator.getName() + " " + currentTerminator.getModel());
-
-            currentTerminator.speak();
-        }
-
-        else if (choice > 3 && choice <= 8) {
+            terminatorAction(choice, currentTerminator);
+        } else if (choice > 3 && choice <= 8) {
             currentTerminator = new TerminatorUpgrade("Punisher", "T-2000", "Machine Gun", "Liquid");
-
-            System.out.println("Random number = " + choice + " so created Terminator is: " + currentTerminator.getName() + " " + currentTerminator.getModel());
-
-            currentTerminator.speak();
-
+            terminatorAction(choice, currentTerminator);
         } else {
             currentTerminator = new TerminatorLatest("Executor", "T-TX", "Rocket Launcher", "Solid");
-
-            System.out.println("Random number = " + choice + " so created Terminator is: " + currentTerminator.getName() + " " + currentTerminator.getModel());
-
-            currentTerminator.speak();
+            terminatorAction(choice, currentTerminator);
         }
+    }
+
+    public static void terminatorAction(int choice, Terminator currentTerminator) {
+        System.out.println("Random number = " + choice + " so created Terminator is: " + currentTerminator.getName() + " " + currentTerminator.getModel());
+        currentTerminator.speak();
     }
 }
