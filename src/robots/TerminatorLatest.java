14db6a48935e10cdda5bf18995edac8da6129555
Nazar Bondarenko <48944609+NazarBondarenkoQ@users.noreply.github.com>
2019-06-23 15:38:26 +0300

    Update TerminatorLatest.java

--- src/robots/TerminatorLatest.java
+++ src/robots/TerminatorLatest.java
@@ -1,12 +1,7 @@
 package robots;
 
-<<<<<<< HEAD
 public class TerminatorLatest extends TerminatorUpgrade {
     private boolean invisibility;
-=======
-    public class TerminatorLatest extends TerminatorUpgrade{
-        private boolean invisibility;
->>>>>>> a77c14681215e7db69e0abab7f5c452b2a35bfb6
 
     public TerminatorLatest(String name, String model, String weapon, String form) {
         super(name, model, weapon, form);
