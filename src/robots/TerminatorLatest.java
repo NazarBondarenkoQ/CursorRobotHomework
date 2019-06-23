a77c14681215e7db69e0abab7f5c452b2a35bfb6
Nazar Bondarenko <48944609+NazarBondarenkoQ@users.noreply.github.com>
2019-06-23 12:16:24 +0300

    Added brackets to the if-else construction.
    Added brackets

--- src/robots/TerminatorLatest.java
+++ src/robots/TerminatorLatest.java
@@ -3,16 +3,16 @@ package robots;
     public class TerminatorLatest extends TerminatorUpgrade{
         private boolean invisibility;
 
-        public TerminatorLatest(String name, String model, String weapon, String form) {
-            super(name, model, weapon, form);
-        }
+    public TerminatorLatest(String name, String model, String weapon, String form) {
+        super(name, model, weapon, form);
+    }
 
-        public void setInvisibility(){
-            invisibility =! invisibility;
-            if (invisibility == true){
-                System.out.println("I'm invisible");
-            }
-            else
-                System.out.println("I'm visible");
+    public void setInvisibility() {
+        invisibility = !invisibility;
+        if (invisibility) {
+            System.out.println("I'm invisible");
+        } else {
+            System.out.println("I'm visible");
         }
     }
+}
