6beed13625121c191de2d67675ecd396c5c42660
Nazar Bondarenko <48944609+NazarBondarenkoQ@users.noreply.github.com>
2019-06-24 21:48:37 +0300

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,7 +11,7 @@ public class Main {
         Random random = new Random();
         final int asciiCharCode = 65; // I use ASCII character encoding to create a random character, "65" stays for "A" char.
         int choice = random.nextInt(12) + 1;
-        int temp = random.nextInt(3) + asciiCharCode; // Here is the usage of constant "66" stays for "B" and "67" for "C".
+        int temp = random.nextInt(3) + asciiCharCode; // Here is the usage of constant "65+1" stays for "B" and "66+1" for "C" relevantly.
         char destination = (char) temp;
         Terminator currentTerminator;
 
